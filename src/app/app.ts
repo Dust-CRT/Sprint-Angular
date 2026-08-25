a7bb764df0c6cdf5d00d25f617d7732f60e80782
Dust-CRT <saascontapessoal@gmail.com>
2026-08-25 08:29:44 -0300

    aula_24/08

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BoasVindas } from "./pages/boas-vindas/boas-vindas";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BoasVindas],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
