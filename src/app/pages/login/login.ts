21b832218e036cd1508b22af5d8776af7c52ab15
Dust-CRT <saascontapessoal@gmail.com>
2026-09-16 11:50:20 -0300

    WIP conexão com API

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Auth } from '../../services/auth';
+import { Router } from '@angular/router';
+import { errorContext } from 'rxjs/internal/util/errorContext';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrl: './login.css',
 })
 export class Login {
-  
+  usuario = {
+    nome: "",
+    senha: ""
+  };
+
+  mensagemDeErro: String | null = null;
+
+  constructor(private auth: Auth, private router: Router) {};
+
+  login() : void {
+    this.auth.login(this.usuario).subscribe({
+      next:(Response) => {
+        this.router.navigate(["/home"]);
+      },
+      error:(err) => {
+        this.mensagemDeErro = err.error.message || "Usuario" 
+      }
+    })
+  }
 }
